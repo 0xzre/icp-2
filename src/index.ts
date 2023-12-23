59d50f447308fb2cba00fdfe8632c174dadedf56
0xzre <alilo.ghazali@gmail.com>
2023-12-23 22:44:13 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import { $query, $update, Record, Result, StableBTreeMap, nat64, ic, match } from 'azle';
+import { v4 as uuidv4 } from 'uuid';
+
+type Paper = Record<{
+    id: string;
+    title: string;
+    authors: string[];
+    abstract: string;
+    contentHash: string; // TODO: think about storing on IPFS
+    submissionDate: nat64;
+}>;
+
+const papers = new StableBTreeMap<string, Paper>(0, 44, 1024);
+
+$update;
+export function submitPaper(title: string, authors: string[], abstract: string, contentHash: string): Result<Paper, string> {
+    const id = uuidv4();
+    const submissionDate = ic.time();
+
+    const newPaper: Paper = {
+        id,
+        title,
+        authors,
+        abstract,
+        contentHash,
+        submissionDate
+    };
+
+    papers.insert(id, newPaper);
+    return Result.Ok<Paper, string>(newPaper);
+}
+
+$query;
+export function getPaper(id: string): Result<Paper, string> {
+    return match(papers.get(id), {
+        Some: (paper) => Result.Ok<Paper, string>(paper),
+        None: () => Result.Err<Paper, string>('Paper not found')
+    });
+}
+
+$query;
+export function listPapers(): Result<Array<Paper>, string> {
+    try {
+        const paperList = papers.values();
+        return Result.Ok(paperList);
+    } catch (error) {
+        return Result.Err('Failed to fetch papers');
+    }
+}
